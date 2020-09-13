8433a082c8101217f4fc4a88b884d3d5a9c91010
patrick <patrickwuke@163.com>
2020-09-13 08:16:50 +0800

    update batch insert

---
+++ qmeta-datafactory/src/main/java/io/qmeta/datafactory/database/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * ToDo List:
+ * 1. Generate Insert Script for table
+ * 2. Batch Insert to database
+ */
+package io.qmeta.datafactory.database;
--- qmeta-datafactory/src/main/java/io/qmeta/datafactory/mock/controller/MockDataController.java
+++ qmeta-datafactory/src/main/java/io/qmeta/datafactory/mock/controller/MockDataController.java
@@ -2,6 +2,9 @@ package io.qmeta.datafactory.mock.controller;
 
 /**
  * To Do list, Make A Mock Data Controller
+ * 1. Project Information Controller
+ * 2. Project API Information Controller
+ * 3. Generate By Configuration
  */
 public class MockDataController {
 }
